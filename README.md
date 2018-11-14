634b77b8583afa18bc6c04511f27c708612ae348
shriram <shriramvenugopal123@gmail.com>
2018-11-14 19:16:36 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+THIS IS A SCRAPY BOT THAT COLLECTS DATA FROM 'www.ralphlauren.com'
+
+TO RUN THE PROGRAM GO TO THE PROJECT FOLDER AND EXECUTE 
+
+    scrapy crawl ralphbot
+
+
+NOTE!!!---------------->
+
+    THE OUTPUT IS ALREADY STORED IN project35/ralphlaure.csv
+
---
+++ project35/spiders/ralphbot.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class RalphbotSpider(scrapy.Spider):
+    name = 'ralphbot'
+    allowed_domains = ['www.ralphlauren.com']
+    start_urls = ['http://www.ralphlauren.com/']
+
+    def parse(self, response):
+        sub_link  = response.css('a[data-cgid]::attr(href)').extract()
+        for page in sub_link :
+            yield scrapy.Request(url=page, callback=self.parse_page, dont_filter=True)    
+
+    def parse_page(self, response):           
+        brand_name = response.css('div.product-tile div.brand-name::text').extract()
+        product_name = response.css('.product-tile .product-name .name-link::text').extract()
+        price = response.css('.product-tile .product-pricing .product-sales-price::text').extract()
+        img_link = response.css('.product-tile .product-image .thumb-link img.default-img ::attr(src)').extract()
+
+        for item in zip(brand_name, product_name, price, img_link):
+            scraped_info = {
+                'brand_name' : item[0],
+                'product_name' : item[1],
+                'price' : item[2],
+                'img_link' : item[3],
+            }
+
+            yield scraped_info
+        
+        next_page = response.css('li.first-lastbar a::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page, callback=self.parse_page, dont_filter=True)
+   
