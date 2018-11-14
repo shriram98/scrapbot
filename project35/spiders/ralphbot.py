b0c9d71845239c8f7b70408a226aa4194680be72
shriram <shriramvenugopal123@gmail.com>
2018-11-14 20:09:55 +0530

    added comments

--- project35/spiders/ralphbot.py
+++ project35/spiders/ralphbot.py
@@ -7,11 +7,14 @@ class RalphbotSpider(scrapy.Spider):
     allowed_domains = ['www.ralphlauren.com']
     start_urls = ['http://www.ralphlauren.com/']
 
+    # This method gets each link from the navbar and sends each link to parse_page
     def parse(self, response):
         sub_link  = response.css('a[data-cgid]::attr(href)').extract()
         for page in sub_link :
             yield scrapy.Request(url=page, callback=self.parse_page, dont_filter=True)    
 
+    # This method iterates over each item in the current page and scrapes the result to a csv file
+    # parse_page goes to the next item page from the current page automatically
     def parse_page(self, response):           
         brand_name = response.css('div.product-tile div.brand-name::text').extract()
         product_name = response.css('.product-tile .product-name .name-link::text').extract()
@@ -30,6 +33,7 @@ class RalphbotSpider(scrapy.Spider):
         
         next_page = response.css('li.first-lastbar a::attr(href)').extract_first()
         if next_page is not None:
-            next_page = response.urljoin(next_page)
+            # urljoin is used to load the next page
+            next_page = response.urljoin(next_page) 
             yield scrapy.Request(url=next_page, callback=self.parse_page, dont_filter=True)
    
